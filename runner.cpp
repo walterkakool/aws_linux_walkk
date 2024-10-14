3d60dd994c43cd843e2d9e2e4ebf9e6f6a38d6fe
walkk <72165225+walterkakool@users.noreply.github.com>
2024-10-14 00:17:00 -0700

    Added runner.cpp

---
+++ runner.cpp
@@ -0,0 +1,27 @@
+#include <aws/lambda-runtime/runtime.h>
+#include <aws/core/utils/json/JsonSerializer.h>
+#include <iostream>
+
+using namespace std;
+using namespace aws::lambda_runtime;
+using namespace Aws::Utils::Json;
+
+invocation_response my_handler(invocation_request const& req) {
+    // Parse the input
+    JsonValue json(req.payload);
+    if (!json.WasParseSuccessful()) {
+        return invocation_response::failure("Invalid JSON", "InvalidJSON");
+    }
+
+    auto view = json.View();
+
+    JsonValue response;
+    cout << "Ran deleteAll successfully.";
+    
+    return invocation_response::success(response.View().WriteCompact(), "application/json");
+}
+
+int main() {
+    run_handler(my_handler);
+    return 0;
+}
