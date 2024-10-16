65a3a1af3114e12d34ccceae6ec2628e6a0a135b
walterkakool <walkk@ip-172-31-39-122.ec2.internal>
2024-10-16 23:04:55 +0000

    Simplest working runner.

--- runner.cpp
+++ runner.cpp
@@ -4,25 +4,14 @@
 using namespace std;
 using namespace aws::lambda_runtime;
 
-/*
-invocation_response my_handler(invocation_request const& req) {
-    // Parse the input
-    JsonValue json(req.payload);
-    if (!json.WasParseSuccessful()) {
-        return invocation_response::failure("Invalid JSON", "InvalidJSON");
-    }
-
-    auto view = json.View();
-
-    JsonValue response;
-    cout << "Ran deleteAll successfully.";
-    
-    return invocation_response::success(response.View().WriteCompact(), "application/json");
+invocation_response my_handler(invocation_request const& request)
+{
+   return invocation_response::success("Walkk suceeded.", "application/json");
 }
-*/
 
 int main() {
-              //run_handler(my_handler);
-
-                return 0;
+            
+              run_handler(my_handler);
+              
+              return 0;
 }
